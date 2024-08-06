14731cccc5bb562bfd26dfcc1bed53931f8d3b84
Наташка Овечкина <natallia.ovechkina@gmail.com>
2024-08-06 20:51:59 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let condition = ''
+  for (i = 0; i < bracketsConfig.length; i++ ) {
+    if (isNaN(+bracketsConfig[i][0])){  
+      condition += `\\${bracketsConfig[i][0]}\\${bracketsConfig[i][1]}|`
+    }
+    else {
+      condition += `${bracketsConfig[i][0]}${bracketsConfig[i][1]}|`
+    }
+  }
+  condition = condition.substring(0, condition.length - 1)
+
+  let result = str.replace(RegExp(condition, 'g'),'');
+   
+    while (result !== result.replace(RegExp(condition, 'g'),'')) {
+    result = result.replace(RegExp(condition, 'g'),'')
+    } 
+    if (result === '') return true;
+    else return false;
 }
